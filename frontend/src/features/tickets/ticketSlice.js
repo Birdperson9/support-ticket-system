59e1665092ada0d671c4275ebaa6a1441fa7be05
Birdperson9 <yorieshkin@gmail.com>
2022-11-04 23:51:20 +0200

    remove isSuccess, isLoading, reset from tickets state

--- frontend/src/features/tickets/ticketSlice.js
+++ frontend/src/features/tickets/ticketSlice.js
@@ -2,11 +2,9 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import ticketService from './ticketService'
 
 const initialState = {
-  tickets: [],
-  ticket: {},
+  tickets: null,
+  ticket: null,
   isError: false,
-  isSuccess: false,
-  isLoading: false,
   message: '',
 }
 
@@ -93,59 +91,42 @@ export const closeTicket = createAsyncThunk(
 export const ticketSlice = createSlice({
   name: 'ticket',
   initialState,
-  reducers: {
-    reset: (state) => initialState,
-  },
   extraReducers: (builder) => {
     builder
-      .addCase(createTicket.pending, (state) => {
-        state.isLoading = true
-      })
-      .addCase(createTicket.fulfilled, (state) => {
-        state.isLoading = false
-        state.isSuccess = true
-      })
       .addCase(createTicket.rejected, (state, action) => {
-        state.isLoading = false
         state.isError = true
         state.message = action.payload
       })
       .addCase(getTickets.pending, (state) => {
-        state.isLoading = true
+        state.ticket = null
       })
       .addCase(getTickets.fulfilled, (state, action) => {
-        state.isLoading = false
-        state.isSuccess = true
         state.tickets = action.payload
+        state.isError = false
+        state.message = ''
       })
       .addCase(getTickets.rejected, (state, action) => {
-        state.isLoading = false
         state.isError = true
         state.message = action.payload
       })
-      .addCase(getTicket.pending, (state) => {
-        state.isLoading = true
-      })
       .addCase(getTicket.fulfilled, (state, action) => {
-        state.isLoading = false
-        state.isSuccess = true
         state.ticket = action.payload
+        state.isError = false
+        state.message = ''
       })
       .addCase(getTicket.rejected, (state, action) => {
-        state.isLoading = false
         state.isError = true
         state.message = action.payload
       })
       .addCase(closeTicket.fulfilled, (state, action) => {
-        state.isLoading = false
-        state.tickets.map((ticket) =>
-          ticket._id === action.payload._id
-            ? (ticket.status = 'closed')
-            : ticket
+        state.ticket = action.payload
+        state.isError = false
+        state.message = ''
+        state.tickets = state.tickets.map((ticket) =>
+          ticket._id === action.payload._id ? action.payload : ticket
         )
       })
   },
 })
 
-export const { reset } = ticketSlice.actions
 export default ticketSlice.reducer
